a1e03b3c2620ab591b86b548f7c5ef04516a05a1
Chuck <chuck30621@gmail.com>
2020-05-02 23:32:53 +0800

    2020.05.02

---
+++ module/OneOfFiveRule.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # import tool
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+
+# # import self-define module
+
+# In[2]:
+
+## 若要在同一級執行要
+# from OnePlusOne import class_OnePlusOne
+from module.OnePlusOne import class_OnePlusOne
+
+
+# In[3]:
+
+
+class class_OneOfFiveRule(class_OnePlusOne):
+    def __init__(self, Gamma_, 
+                 Parent_ = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+                 iteration_ = 10000000):
+        super().__init__(Gamma_, Parent_, iteration_)
+        
+        self.Gs = 0
+    
+    def selection(self):
+        if( self.fit_func(self.Child) <  self.fit_func(self.Parent) ):
+            self.Parent = self.Child
+            self.Gs = self.Gs + 1
+    
+    def Minimization(self):
+        for i in tqdm(range(self.iteration)):
+            if( self.fit_func(self.Parent) < 0.005):
+                break
+            self.mutation()
+            self.selection()
+            
+            if( self.Gs/(i+1) < 0.2):
+                self.Gamma = self.Gamma*0.85
+            elif( self.Gs/(i+1) > 0.2):
+                self.Gamma = self.Gamma/0.85
+            
+        self.result = self.Parent
+        self.result_fit = self.fit_func(self.Parent)
+        self.time = i    
+
+
+# In[4]:
+
+
+# for i in tqdm(range(10)):
+#     print("\nRun %d Start:" % (i+1))
+#     test = Rule(1)
+#     test.Minimization()
+#     print("fit Parent: %s" % test.result)
+#     print("fit value: %.6f" % test.result_fit)
+#     print("iteration: %d" % test.time)
+
---
+++ module/OnePlusOne.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # importy tool
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+
+# In[2]:
+
+
+class class_OnePlusOne:
+    def __init__(self, Gamma_, 
+                 Parent_ = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+                 iteration_ = 10000000):
+        self.Gamma = Gamma_
+        self.Parent = Parent_
+        self.iteration = iteration_ 
+        # self.child
+        
+    def fit_func(self, list_):
+        buf = [i**2 for i in list_]
+        return( sum(buf) )
+    
+    def mutation(self):
+        delta = np.random.normal(0, self.Gamma, len(self.Parent))
+        self.Child = list(delta + self.Parent)
+    
+    def selection(self):
+        if( self.fit_func(self.Child) <  self.fit_func(self.Parent) ):
+            self.Parent = self.Child
+        
+    def Minimization(self):
+        for i in tqdm(range(self.iteration)):
+            if( self.fit_func(self.Parent) < 0.005):
+                break
+            self.mutation()
+            self.selection()
+        self.result = self.Parent
+        self.result_fit = self.fit_func(self.Parent)
+        self.time = i
+
+
+# In[3]:
+
+
+# test = ES(0.01, iteration_ = 10000)
+
+# test.Minimization()
+
+# print(test.result)
+# print(test.result_fit)
+# print(test.time)
+
